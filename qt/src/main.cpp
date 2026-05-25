881e44719f0bbafcf6c36833fc2222d5800509dc
Pingancoin <1852287928@qq.com>
2026-05-25 12:27:15 +0800

    Add native Qt wallet app icons

--- qt/src/main.cpp
+++ qt/src/main.cpp
@@ -1,12 +1,14 @@
 #include "MainWindow.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     app.setApplicationName(QStringLiteral("Pingancoin Wallet"));
     app.setOrganizationName(QStringLiteral("Pingancoin"));
+    app.setWindowIcon(QIcon(QStringLiteral(":/icons/pingancoin-icon-256.png")));
 
     pacqt::MainWindow window;
     window.show();
